ce48ffff0951a93692177ffc33558a70cf85019b
eric <ericlam@vitessedata.com>
2023-11-07 16:45:07 +0000

    optional type

--- salesreceipt.go
+++ salesreceipt.go
@@ -10,23 +10,23 @@ import (
 type SalesReceipt struct {
 	ID            string   `json:"Id,omitempty"`
 	SyncToken     string   `json:",omitempty"`
-	MetaData      MetaData `json:",omitempty"`
+	MetaData      *MetaData `json:",omitempty"`
 	DocNumber     string   `json:",omitempty"`
-	TxnDate       Date     `json:",omitempty"`
+	TxnDate       *Date     `json:",omitempty"`
 	PrivateNote   string   `json:",omitempty"`
 	Line          []Line
-	BillAddr      PhysicalAddress `json:",omitempty"`
-	ShipAddr      PhysicalAddress `json:",omitempty"`
-	ShipFromAddr  PhysicalAddress `json:",omitempty"`
+	BillAddr      *PhysicalAddress `json:",omitempty"`
+	ShipAddr      *PhysicalAddress `json:",omitempty"`
+	ShipFromAddr  *PhysicalAddress `json:",omitempty"`
 	PaymentRefNum string          `json:",omitempty"`
 	Domain        string          `json:"domain,omitempty"`
 
-	ShipMethodRef ReferenceType `json:",omitempty"`
-	ShipDate      Date          `json:",omitempty"`
+	ShipMethodRef *ReferenceType `json:",omitempty"`
+	ShipDate      *Date          `json:",omitempty"`
 	TrackingNum   string        `json:",omitempty"`
 	TotalAmt      json.Number   `json:",omitempty"`
 
-	CurrencyRef           ReferenceType `json:",omitempty"`
+	CurrencyRef           *ReferenceType `json:",omitempty"`
 	ExchangeRate          json.Number   `json:",omitempty"`
 	PrintStatus           string        `json:",omitempty"` // Valid values: NotSet, NeedToPrint, PrintComplete
 	EmailStatus           string        `json:",omitempty"`
@@ -34,17 +34,17 @@ type SalesReceipt struct {
 	FreeFormAddress       bool          `json:",omitempty"`
 	ApplyTaxAfterDiscount bool          `json:",omitempty"`
 	Spare                 bool          `json:"spare,omitempty"`
-	DepositToAccountRef   ReferenceType `json:",omitempty"`
-	CustomerMemo          MemoRef       `json:",omitempty"`
-	ProjectRef            ReferenceType `json:",omitempty"`
+	DepositToAccountRef   *ReferenceType `json:",omitempty"`
+	CustomerMemo          *MemoRef       `json:",omitempty"`
+	ProjectRef            *ReferenceType `json:",omitempty"`
 
 	Balance          json.Number   `json:",omitempty"`
-	CustomerRef      ReferenceType `json:",omitempty"`
-	TxnTaxDetail     TxnTaxDetail  `json:",omitempty"`
-	PaymentMethodRef ReferenceType `json:",omitempty"`
+	CustomerRef      *ReferenceType `json:",omitempty"`
+	TxnTaxDetail     *TxnTaxDetail  `json:",omitempty"`
+	PaymentMethodRef *ReferenceType `json:",omitempty"`
 
 	GlobalTaxCalculation string        `json:",omitempty"` // Values: TaxExcluded, TaxInclusive, NotApplicable
-	DepositToAccount     ReferenceType `json:",omitempty"`
+	DepositToAccount     *ReferenceType `json:",omitempty"`
 }
 
 func (c *Client) CreateSalesReceipt(receipt *SalesReceipt) (*SalesReceipt, error) {
